76c864eb94bd8df9aba32af08d365b017f57da44
Yasir Ameen <Yasirameen92@gmail.com>
2024-05-31 18:58:05 +0500

    new logo update

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(360, 800),
       builder: ((context, child) => GetMaterialApp(
-            title: 'Lussoro Man',
+            title: 'Lussoro Brand',
             debugShowCheckedModeBanner: false,
             translations: Languages(),
             locale: langValue,
